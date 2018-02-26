9e6907be31e3df35aa18307ab396f9b0678b4d8b
David Marchante <davidmarchan@gmail.com>
2018-02-26 13:21:47 +0100

    Changed generation of sampleCards

--- src/sampleCards.js
+++ src/sampleCards.js
@@ -1,25 +1,34 @@
-const cards = {
-  1: {
-    body: 'Card 1',
+import Card from './models/Card';
+
+const cardList = [
+  new Card('Card1', {
     x: 123,
     y: 234,
     width: 123,
     height: 90,
-  },
-  2: {
-    body: 'Card 2',
+  }),
+  new Card('Card2', {
     x: 321,
     y: 432,
     width: 223,
     height: 80,
-  },
-  3: {
-    body: 'Card 3',
+  }),
+  new Card('Card3', {
     x: 623,
     y: 142,
     width: 142,
     height: 111,
-  }
-};
+  })
+];
+
+// { [id]: [Card]
+// As this is too fast to generate unique ids based on the date
+// I'll create new ones
+let id = 0;
+const cards = cardList.reduce((cards, card) => {
+  card.id = ++id;
+  cards[card.id] = card;
+  return cards;
+}, {});
 
 export default cards;
