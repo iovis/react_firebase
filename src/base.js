37aa76b2742bb645579c42ab7776107b5aec5954
David Marchante <davidmarchan@gmail.com>
2018-03-05 08:10:07 +0100

    Refactored base.js

--- src/base.js
+++ src/base.js
@@ -1,14 +1,13 @@
 const Rebase = require('re-base');
 const firebase = require('firebase');
 
-const app = firebase.initializeApp({
+const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 });
 
-const base = Rebase.createClass(app.database());
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
 export default base;
